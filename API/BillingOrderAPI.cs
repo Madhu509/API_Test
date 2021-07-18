6c53cb9b87bad1ba4f5e16186c2733364e7a8dce
Madhu509 <86769422+Madhu509@users.noreply.github.com>
2021-07-19 02:59:15 +1200

    initial commit

---
+++ API/BillingOrderAPI.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using RestSharp;
+
+namespace BillingOrders.API
+{
+    public class BillingOrderAPI
+    {
+        private readonly string baseUrl = "http://localhost:8080";
+
+        public IRestResponse GetOrdersById(string id)
+        {
+            //setting things
+            var client = new RestClient($"{baseUrl}/BillingOrder/{id}");
+            var request = new RestRequest(Method.GET);
+
+            //Execution
+            IRestResponse response = client.Execute(request);
+
+            return response;
+
+        }
+
+        public IRestResponse GetAllRecords()
+        {
+            //setting things
+            var client = new RestClient($"{baseUrl}/BillingOrder");
+            var request = new RestRequest(Method.GET);
+
+            //Execution
+            IRestResponse response = client.Execute(request);
+
+            return response;
+
+        }
+
+        public IRestResponse DeleteOrderById(string id)
+        {
+            //setting things
+            var client = new RestClient($"{baseUrl}/BillingOrder/{id}");
+            var request = new RestRequest(Method.DELETE);
+
+            //Execution
+            IRestResponse response = client.Execute(request);
+
+            return response;
+
+        }
+
+        public IRestResponse PostOrder(string body)
+        {
+            //setting things
+            var client = new RestClient($"{baseUrl}/BillingOrder");
+            var request = new RestRequest(Method.POST);
+            //header
+            request.AddHeader("Content-Type", "application/json");
+            //post
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+
+            //Execution
+            IRestResponse response = client.Execute(request);
+
+            return response;
+
+        }
+        public IRestResponse PutOrder(string id, string body)
+        {
+            //setting things
+            var client = new RestClient($"{baseUrl}/BillingOrder/{id}");
+            var request = new RestRequest(Method.PUT);
+            //header
+            request.AddHeader("Content-Type", "application/json");
+            //post
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+
+            //Execution
+            IRestResponse response = client.Execute(request);
+
+            return response;
+
+        }
+    }
+}
---
+++ APITests.cs
@@ -0,0 +1,79 @@
+﻿using BillingOrders.API;
+using NUnit.Framework;
+using RestSharp;
+
+namespace BillingOrders
+{
+    public class Tests
+    {
+
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void TC_GetOrderById()
+        {
+            BillingOrderAPI billingOrder = new BillingOrderAPI();
+            IRestResponse response = billingOrder.GetOrdersById("10");
+            TestContext.WriteLine(response.Content);
+
+            //Assertions pending
+        }
+        [Test]
+        public void TC_DeleteOrder()
+        {
+            BillingOrderAPI billingOrder = new BillingOrderAPI();
+            IRestResponse response = billingOrder.DeleteOrderById("2");
+            TestContext.WriteLine(response.Content);
+
+            //Assertions pending
+        }
+
+        [Test]
+        public void TC_PostOrder()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                string body = $"{{\"addressLine1\":\"auckland{i}\", \"addressLine2\":\"nz{i}\", \"city\":\"penrose\", \"comment\":\"testing\", \"email\":\"madhurima.atla@gmail.com\", \"firstName\":\"naina{i}\", \"itemNumber\":0, \"lastName\":\"atla{i}\", \"phone\":\"0223065400\", \"state\":\"AL\", \"zipCode\":\"123123\"}}";
+
+                BillingOrderAPI billingOrder = new BillingOrderAPI();
+
+                IRestResponse response = billingOrder.PostOrder(body);
+                TestContext.WriteLine(response.Content);
+
+                //Assertions pending
+            }
+        }
+
+        [Test]
+        public void TC_GetAllRecords()
+        {
+            BillingOrderAPI billingOrder = new BillingOrderAPI();
+            IRestResponse response = billingOrder.GetAllRecords();
+            TestContext.WriteLine(response.Content);
+
+            //Assertions pending
+        }
+        [Test]
+        public void TC_PutOrder()
+        {
+            string body = $"{{\"addressLine1\":\"auckland\", \"addressLine2\":\"nz\", \"city\":\"penrose\", \"comment\":\"testing\", \"email\":\"madhurima.atla@gmail.com\", \"firstName\":\"naina\", \"itemNumber\":0, \"lastName\":\"atla\", \"phone\":\"0223065400\", \"state\":\"AL\", \"zipCode\":\"123123\"}}";
+
+            BillingOrderAPI billingOrder = new BillingOrderAPI();
+
+            //pending
+
+            IRestResponse response = billingOrder.PutOrder("100", body);
+            TestContext.WriteLine(response.Content);
+
+            //Assertions pending
+        }
+
+
+    }
+}
+
